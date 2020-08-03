52a07e4fb1346b9690796c1c8ecf8137b2081ad9
nay9 <renenoe94@hotmail.com>
2020-08-03 18:41:09 -0400

    pushing up part of FIgureWindow class

--- src/com/company/FigureWindow.java
+++ src/com/company/FigureWindow.java
@@ -1,4 +1,21 @@
 package com.company;
 
-public class FigureWindow {
+import java.awt.*;
+
+public class FigureWindow extends Frame {
+    private int numOfFigures;
+    private Figure[] figures;
+
+    public FigureWindow(int max) {
+        super("Fabulus Figures");
+        figures = new Figure[max];
+        numOfFigures = 0;
+    }
+
+    public void addFigure(Figure f) {
+        if (numOfFigures < figures.length) {
+            figures[numOfFigures] = f;
+            numOfFigures++;
+        }
+    }
 }
