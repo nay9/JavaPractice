5fa2406fb2648cf9273d6cabc4af24da48c1710c
nay9 <renenoe94@hotmail.com>
2020-08-06 15:22:42 -0400

    documented a bunch of stuff

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -2,12 +2,16 @@ package com.company;
 import java.awt.*;
 
 public class Circle extends Figure {
+
     private int d;
+
+    // inX and inY are coordinates, inDiam is the diameter of the circle
     public Circle(int inX, int inY, int inDiam) {
         super(inX, inY);
         d = inDiam;
     }
 
+    // method to draw circle
     public void draw(Graphics g){
         g.drawOval(x,y,d,d);
     }
--- src/com/company/Square.java
+++ src/com/company/Square.java
@@ -2,12 +2,16 @@ package com.company;
 import java.awt.*;
 
 abstract class Square extends Figure {
+
     private int side;
+
+    //inX and inY are coordinates, inSide is the length of the square
     public Square(int inX, int inY, int inSide) {
         super(inX,inY);
         side = inSide;
     }
 
+    // drawing method for the square
     public void draw(Graphics g) {
         g.drawRect(x, y, side, side);
     }
